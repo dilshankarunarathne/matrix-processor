40bb5bdaba7a41b0fa3283fdc2c49176857ca0b4
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-18 20:44:31 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,30 @@ int main() {
     return 0;
 }
 
-/**
- * adds two matrices
- */
-void add(int a[3][3], int b[3][3], int c[3][3]) {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            c[i][j] = a[i][j] + b[i][j];
+class Matrix {
+    private:
+        int rows;
+        int cols;
+        int **matrix;
+    public:
+        Matrix(int rows, int cols);
+        ~Matrix();
+        void set(int row, int col, int value);
+        int get(int row, int col);
+        void print();
+
+    /**
+     * adds two Matrices and returns the result
+     */
+    Matrix add(Matrix m, Matrix n);
+};
+
+Matrix Matrix::add(Matrix m, Matrix n) {
+    Matrix result(m.rows, m.cols);
+    for (int i = 0; i < m.rows; i++) {
+        for (int j = 0; j < m.cols; j++) {
+            result.set(i, j, m.get(i, j) + n.get(i, j));
         }
     }
+    return result;
 }
