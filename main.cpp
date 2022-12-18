e750933f30e2f59f4889c0c277ba4d0dc9755194
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-18 20:44:52 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 
+class Matrix {
+public:
+    Matrix(int rows, int cols) : rows(rows), cols(cols) {
+        data = new int[rows * cols];
+    }
+
+    ~Matrix() {
+        delete[] data;
+    }
+
+    int& operator()(int row, int col) {
+        return data[row * cols + col];
+    }
+
+    int operator()(int row, int col) const {
+        return data[row * cols + col];
+    }
+};
+
 int main() {
 
     return 0;
 }
 
-class Matrix 
