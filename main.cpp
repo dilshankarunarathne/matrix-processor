06c83a08aa62fa016a6120148723c7f197d36eac
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-18 20:33:39 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
+}
+
+/**
+ * adds two matrices
+ */
+void add(int a[3][3], int b[3][3], int c[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
 }
