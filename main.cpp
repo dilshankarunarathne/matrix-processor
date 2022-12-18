5e48819f36d22295cf5f63c1a38000d11cb22ec4
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-18 21:25:23 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -116,6 +116,20 @@ public:
         }
         return result;
     }
+
+    /**
+     * @brief Prints the matrix
+     * @details This method prints the matrix to the standard
+     *       output stream.
+     */
+    void print() const {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                std::cout << m[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
 };
 
 int main() {
