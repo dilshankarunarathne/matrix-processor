af79bb9051e723356640a5e0a80d120add83a9e8
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2022-12-18 20:45:44 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,66 @@
 #include <iostream>
 
+/**
+ * @brief Implements a three-by-three matrix
+ */
 class Matrix {
 public:
-    Matrix(int rows, int cols) : rows(rows), cols(cols) {
-        data = new int[rows * cols];
+    /**
+     * @brief Default constructor
+     */
+    Matrix() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                m[i][j] = 0;
+            }
+        }
     }
 
-    ~Matrix() {
-        delete[] data;
+    /**
+     * @brief Constructor
+     * @param a00 First element of the first row
+     * @param a01 Second element of the first row
+     * @param a02 Third element of the first row
+     * @param a10 First element of the second row
+     * @param a11 Second element of the second row
+     * @param a12 Third element of the second row
+     * @param a20 First element of the third row
+     * @param a21 Second element of the third row
+     * @param a22 Third element of the third row
+     */
+    Matrix(int a00, int a01, int a02, int a10, int a11, int a12, int a20, int a21, int a22) {
+        m[0][0] = a00;
+        m[0][1] = a01;
+        m[0][2] = a02;
+        m[1][0] = a10;
+        m[1][1] = a11;
+        m[1][2] = a12;
+        m[2][0] = a20;
+        m[2][1] = a21;
+        m[2][2] = a22;
     }
 
-    int& operator()(int row, int col) {
-        return data[row * cols + col];
+    /**
+     * @brief Copy constructor
+     * @param other The matrix to copy
+     */
+    Matrix(const Matrix& other) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                m[i][j] = other.m[i][j];
+            }
+        }
     }
 
-    int operator()(int row, int col) const {
-        return data[row * cols + col];
-    }
+    /**
+     * @brief Assignment operator
+     * @param other The matrix to copy
+     * @return A reference to this matrix
+     */
+    Matrix& operator=(const Matrix& other) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                m[i][j] =
 };
 
 int main() {
