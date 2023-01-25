469b79a1beb0bbb3563ccf639e3a5903bce6afdf
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-01-25 19:26:08 +0530

    Update cMatrix.h

--- cMatrix.h
+++ cMatrix.h
@@ -1,14 +1,43 @@
-//
-// Created by Dilshan Karunarathne on 1/25/2023.
-//
 
 #ifndef MATRIX_PROCESSOR_CMATRIX_H
 #define MATRIX_PROCESSOR_CMATRIX_H
 
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 class cMatrix {
+private:
+    int n;
+    double** m;
+public:
+    cMatrix(int n);
+    cMatrix(const cMatrix& rhs);
+    ~cMatrix();
 
+    cMatrix& operator=(const cMatrix& rhs);
+    cMatrix& operator+=(const cMatrix& rhs);
+    cMatrix& operator-=(const cMatrix& rhs);
+    cMatrix& operator*=(const cMatrix& rhs);
+    cMatrix& operator*=(const int scalar);
+
+    friend cMatrix operator+(const cMatrix& m1, const cMatrix& m2);
+    friend cMatrix operator-(const cMatrix& m1, const cMatrix& m2);
+    friend cMatrix operator*(const cMatrix& m1, const cMatrix& m2);
+    friend cMatrix operator*(const cMatrix& m, const int scalar);
+    friend cMatrix operator*(const int scalar, const cMatrix& m);
+
+    friend istream& operator>>(istream& in, cMatrix& matrix);
+    friend ostream& operator<<(ostream& out, const cMatrix& matrix);
+
+    void set(int i, int j, double val);
+    double get(int i, int j) const;
+    int getSize() const;
+    cMatrix transpose() const;
+    cMatrix adjoin() const;
+    cMatrix inverse() const;
+    int rank() const;
 };
-
 
 #endif //MATRIX_PROCESSOR_CMATRIX_H
