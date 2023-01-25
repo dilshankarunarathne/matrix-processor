49d4392d619dfbbd5fc0ada1fb8f8f5b484908e9
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-01-25 19:40:39 +0530

    new

---
+++ newCMatrix.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dilshan Karunarathne on 1/25/2023.
+//
+
+#include "newCMatrix.h"
---
+++ newCMatrix.h
@@ -0,0 +1,14 @@
+//
+// Created by Dilshan Karunarathne on 1/25/2023.
+//
+
+#ifndef MATRIX_PROCESSOR_NEWCMATRIX_H
+#define MATRIX_PROCESSOR_NEWCMATRIX_H
+
+
+class newCMatrix {
+
+};
+
+
+#endif //MATRIX_PROCESSOR_NEWCMATRIX_H
