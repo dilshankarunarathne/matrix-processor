3a869102c19edf99f112ad4bdcd5e256a41c1b26
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-01-25 19:44:01 +0530

    Update newCMatrix.cpp

--- newCMatrix.cpp
+++ newCMatrix.cpp
@@ -1,5 +1,35 @@
-//
-// Created by Dilshan Karunarathne on 1/25/2023.
-//
+/*
+ * write a program that enables user to enter two matrices of dimension nxn, where n is any positive integer. then the program displays the sum, difference, multiplication, transpose matrices of entered matrices. object oriented principles shoul be applied to solve the programming poroblem with the following features.
+ * a class to represent nxn matrix
+ * appropriate constructions
+ * overridden default constructor
+ * overloaded constructors
+ * copy constructors
+ * overloaded operators
+ * operators should support chaining
+ * garbage collection to handle all temporary objects, enabling proper chaining of overloaded operators, copy constructor.
+ * all this is defined in newCMatrix.h
+ */
 
 #include "newCMatrix.h"
+
+newCMatrix::newCMatrix(int n) {
+    this->n = n;
+    m = new double*[n];
+    for (int i = 0; i < n; i++) {
+        m[i] = new double[n];
+    }
+}
+
+newCMatrix::newCMatrix(const newCMatrix &rhs) {
+    n = rhs.n;
+    m = new double*[n];
+    for (int i = 0; i < n; i++) {
+        m[i] = new double[n];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            m[i][j] = rhs.m[i][j];
+        }
+    }
+}
